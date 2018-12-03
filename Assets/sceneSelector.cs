3315eee77ffb9fb2d62699c5d3e73058b30e985a
cwasserman1 <coolcole0225@gmail.com>
2018-12-02 19:32:56 -0500

    7:32 PM 12/2/18 Start Menu and Scene Manager
    Created the Start Menu. Made a Scene Manager

---
+++ Assets/sceneSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSelector : MonoBehaviour
+{
+    public int sceneID;
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
